907101d701b3585ddb7a45f03765e4877e221abf
AadyaP <77777874+AadyaP@users.noreply.github.com>
2021-04-26 16:56:02 +0530

    Added sound and changed colour of the ball.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 var block1 , block2 , block3 , block4 ;
-var ball;
+var ball;var edges;var music;
 
+function preload(){
+music=loadSound("music.mp3")
+}
 function setup() {
   createCanvas(1200,800);
   block1 = createSprite(600, 400, 50, 80);
@@ -16,59 +19,53 @@ function setup() {
   block3.debug = true;
   block4.debug = true;
   ball = createSprite(400,200,80,30);
-  ball.shapeColor = "green";
+  ball.shapeColor = "pink";
   ball.debug = true;
+  ball.velocityX=2;
+  ball.velocityY=-4; 
+
 }
 
 function draw() {
   background(0,0,0);  
-  ball.x = World.mouseX;
-  ball.y = World.mouseY;
+  
+  edges=createEdgeSprites();
+  ball.bounceOff(edges);
 
   if (ball.x - block1.x < block1.width/2 + ball.width/2
       && block1.x - ball.x < block1.width/2 + ball.width/2
       && ball.y - block1.y < block1.height/2 + ball.height/2
       && block1.y - ball.y < block1.height/2 + ball.height/2) {
-    ball.shapeColor = "red";
-    block1.shapeColor = "red";
+        ball.shapeColor = "green";
+        music.play();
+      
   }
-  else {
-    ball.shapeColor = "green";
-    block1.shapeColor = "green";
-  }
-  if (ball.x - block2.x < block2.width/2 + ball.width/2
+  
+ 
+  else if (ball.x - block2.x < block2.width/2 + ball.width/2
     && block2.x - ball.x < block2.width/2 + ball.width/2
     && ball.y - block2.y < block2.height/2 + ball.height/2
     && block2.y - ball.y < block2.height/2 + ball.height/2) {
-  ball.shapeColor = "red";
-  block2.shapeColor = "red";
+  ball.shapeColor = "blue";
+  music.play();
 }
-else {
-  ball.shapeColor = "green";
-  block2.shapeColor = "blue";
-}
-if (ball.x - block3.x < block3.width/2 + ball.width/2
+
+else if (ball.x - block3.x < block3.width/2 + ball.width/2
   && block3.x - ball.x < block3.width/2 + ball.width/2
   && ball.y - block3.y < block3.height/2 + ball.height/2
   && block3.y - ball.y < block3.height/2 + ball.height/2) {
 ball.shapeColor = "red";
-block3.shapeColor = "red";
+music.play();
 }
-else {
-ball.shapeColor = "green";
-block3.shapeColor = "red";
-}
-if (ball.x - block4.x < block4.width/2 + ball.width/2
+
+else if (ball.x - block4.x < block4.width/2 + ball.width/2
   && block4.x - ball.x < block4.width/2 + ball.width/2
   && ball.y - block4.y < block4.height/2 + ball.height/2
   && block4.y - ball.y < block4.height/2 + ball.height/2) {
-ball.shapeColor = "red";
-ball.shapeColor = "red";
-block4.shapeColor = "red";
+ball.shapeColor = "yellow";
+music.play();
 }
-else {
-ball.shapeColor = "green";
-block4.shapeColor = "yellow";
-}
+else{music.stop()}
+
   drawSprites();
 }
